fa2a9eb2d5d588e3dc8e02e8b11086110843522b
JackieTruongZ <datnguyentruongnn@gmail.com>
2023-11-12 12:36:18 +0700

    change framework backend to NestJs use prisma like ORM

---
+++ frontend/app/cuong/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+function page() {
+  return (
+    <div>
+        <h1>
+            toi ten cuong
+        </h1>
+        <Link href= "../dat/duy">trang cua duy</Link>
+    </div>
+  )
+}
+
+export default page
---
+++ frontend/app/dat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        
+    <h1> day la trang cua dat</h1>
+    </div>
+  )
+}
+
+export default page
