cb1bec9c459a915822adb1edc10736fb7eef692e
JackieTruongZ <datnguyentruongnn@gmail.com>
2023-11-29 14:42:51 +0700

    return frontend :))

--- frontend/app/LogIn/page.tsx
+++ frontend/app/LogIn/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import './style.css'; // Import the CSS file
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-import { log } from 'console';
-import { lockfilePatchPromise } from 'next/dist/build/swc';
 
 
 
@@ -16,36 +14,13 @@ const LogIn = () => {
   // Your component logic here
   const [usernameValue, setUserNameValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
-  const [value, setValue] = useState<string>('');
-
-  const Login = (username: string, password: string) => {
-        // validate
-        if(username == "" || password == "") {
-          // thong bao 
-          console.log("chua nhap username password");
-          
-        } else {
-          console.log(username);
-        console.log(password);
-        
-          
-        }
-
-        // call API
-  }
-
-  useEffect(() => {
-    Login(usernameValue,passwordValue);
-  },[usernameValue,passwordValue]); 
-
   return (
-    <div className="log-in ">
-      <div className="grid">
-        <div className="col-12 md:col-6 lg:col-3">
+    <div className="log-in">
+      <div className="div">
+        <div className="login-form">
           <div className="text-wrapper">Log In</div>
           <div className="username-or-email">
             <div className="text-wrapper-2">Email</div>
-      
             <div className="card flex justify-content-center" >
               <InputText
                 className="username-content"
@@ -67,27 +42,34 @@ const LogIn = () => {
               />
 
             </div>
-    
+
 
             <Link href="" className="text-wrapper-3">Forgot Password?</Link>
           </div>
           <div className="login-button">
             <div className="overlap-group">
               <div className="card flex justify-content-center">
-                <Button label="Log In" onClick={()=> {Login(usernameValue,passwordValue);}}/>
+                <Button label="Log In" />
               </div>
             </div>
           </div>
-
         </div>
-        <div className="col-12 md:col-6 lg:col-3">
         <img className="log-in-image" alt="Log in image" src="./asset/log-in-img.png" />
-        </div>
-       
+        <header className="header">
+          <div className="overlap">
+            <div className="logo-and-log-in-box" />
+            <div className="logo">
+              <div className="website-name">MAGICPOST</div>
+              <img className="website-logo" alt="Website logo" src="website-logo.svg" />
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="div-wrapper">
+                <div className="text-wrapper-5">Log In</div>
+              </div>
+            </div>
+          </div>
+        </header>
       </div>
-
-  
-
 
     </div>
 
@@ -95,4 +77,3 @@ const LogIn = () => {
 };
 
 export default LogIn;
-
