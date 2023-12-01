039f723fb9303b1ff4b8c262ca96dd7ebe5d39b5
= <sayyhello380@gmail.com>
2023-12-01 23:21:39 +0700

    responsive update for some devices not too special one

--- frontend/app/components/MyForm.tsx
+++ frontend/app/components/MyForm.tsx
@@ -3,20 +3,33 @@ import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
 import * as Yup from 'yup';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import "../componentStyle/MyForm.css"
 
 const validationSchema = Yup.object().shape({
+  grantAccess: Yup.boolean().oneOf([true], 'Grant access is required'),
   username: Yup.string().required('Username is required'),
   email: Yup.string().email('Invalid email address').required('Email is required'),
+  employeeType: Yup.string().required('Employee type is required'),
 });
 
 const initialValues = {
   username: '',
   email: '',
+  employeeType: '',
 };
 
-const MyForm = () => {
+const employeeTypes = [
+  { label: 'Normal Employee', value: '1' },
+  { label: 'Outstanding Employee', value: '2' },
+  { label: 'Boss', value: '3' },
+];
+
+export default function MyForm() {
   const handleSubmit = (values, { setSubmitting }) => {
     // Gửi dữ liệu nếu hợp lệ
     console.log('Form submitted:', values);
@@ -30,6 +43,8 @@ const MyForm = () => {
       onSubmit={handleSubmit}
     >
       <Form>
+        <span className='form-name'>Grant Access</span>
+
         <div className="p-field">
           <label htmlFor="username">Username</label>
           <Field
@@ -38,7 +53,6 @@ const MyForm = () => {
             type="text"
             as={InputText}
             className="p-inputtext"
-            placeholder="enter name"
           />
           <ErrorMessage name="username" component="small" className="p-error" />
         </div>
@@ -55,10 +69,24 @@ const MyForm = () => {
           <ErrorMessage name="email" component="small" className="p-error" />
         </div>
 
+        <div className="p-field">
+          <label htmlFor="employeeType">Employee Type</label>
+          <Field
+            id="employeeType"
+            name="employeeType"
+            options={employeeTypes}
+            optionLabel="label"
+            optionValue="value"
+            className="p-dropdown"
+            placeholder="Select an employee type"
+            as={Dropdown}
+          />
+          <ErrorMessage name="employeeType" component="small" className="p-error" />
+        </div>
+
         <Button type="submit" label="Submit" className="p-button p-component" />
       </Form>
     </Formik>
   );
 };
 
-export default MyForm;
