802de096c09ffae57aa71b45207bf134df09c83b
JackieTruongZ <149960670+JackieTruongZ@users.noreply.github.com>
2023-12-12 23:22:17 +0700

    Update BaseService.ts

--- frontend/app/service/BaseService.ts
+++ frontend/app/service/BaseService.ts
@@ -2,8 +2,8 @@ import axios, { AxiosRequestConfig } from "axios";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-// const API_URL = "https://magicpost-60b7.onrender.com";
-const API_URL = "http://localhost:3333";
+const API_URL = "https://magicpost-60b7.onrender.com";
+// const API_URL = "http://localhost:3333";
 
 export class BaseService {
   async login(formData: any) {
