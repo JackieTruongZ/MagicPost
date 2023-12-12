61d83eb370b9427f7348b1f10b78176cad20e331
= <sayyhello380@gmail.com>
2023-12-12 17:19:55 +0700

    start creat result table but not complete

---
+++ frontend/app/dashboard/ResultTable.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react';
+import './resultTableStyle.css';
+
+export default function ResultTable() {
+    return (
+        <div>
+        <h1>result table</h1>
+        </div>
+    );
+}
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FindOrder from './FindOrder';
+import ResultTable from './ResultTable';
 
 function Dashboard() {
   return (
     <div>
       <FindOrder/>
+      <ResultTable></ResultTable>
     </div>
   )
 }
