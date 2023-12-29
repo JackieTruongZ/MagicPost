001a90044c70b9a14b97282e913edf1dc64d0933
= <sayyhello380@gmail.com>
2023-12-29 14:36:13 +0700

    change icon

--- frontend/app/components/bottomBar/Menu.tsx
+++ frontend/app/components/bottomBar/Menu.tsx
@@ -9,6 +9,8 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import DatasetIcon from '@mui/icons-material/Dataset';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
 
 interface Props {
   roleId: string;
@@ -28,52 +30,48 @@ const Menu = ({ roleId }: Props) => {
                 window.location.href = "/director/pointmanager";
               else if (newValue == 1)
                 window.location.href = "/director/accountmanager";
-                else window.location.href = "/PointManager/ordermanager";
+                else window.location.href = "/director/ordermanager";
               setValue(newValue);
             }}
           >
             <BottomNavigationAction label="Point" icon={<LocationOnIcon />} />
-            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Account" icon={<SupervisedUserCircleIcon />} />
             <BottomNavigationAction label="Order" icon={<DatasetIcon />} />
           </BottomNavigation>
         </Box>
       )}
 
       {["51", "52"].includes(roleId) && (
-        <div className="h-full flex flex-row">
-          <MenuItem
-            onClick={() => {
+        <Box>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            if (newValue == 0)
               window.location.href = "/PointManager/accountmanager";
-            }}
-          >
-            <div>
-              <p>Account</p>
-            </div>
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              window.location.href = "/PointManager/ordermanager";
-            }}
-          >
-            <div>
-              <p>Order</p>
-            </div>
-          </MenuItem>
-        </div>
+            else window.location.href = "/PointManager/ordermanager";
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Account" icon={<SupervisedUserCircleIcon />} />
+          <BottomNavigationAction label="Order" icon={<DatasetIcon />} />
+        </BottomNavigation>
+      </Box>
       )}
 
       {["511", "512", "521"].includes(roleId) && (
-        <div className="h-full">
-          <MenuItem
-            onClick={() => {
-              window.location.href = "/Staff";
-            }}
-          >
-            <div>
-              <p style={{ width: "max-content" }}>Làm việc thôi</p>
-            </div>
-          </MenuItem>
-        </div>
+        <Box>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            window.location.href = "/Staff";
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Có làm thì mới có ăn" icon={<PendingActionsIcon />} />
+        </BottomNavigation>
+      </Box>
       )}
     </div>
   );
